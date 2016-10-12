25b66e44b030dcce5893a2d839ca943a229faffc
Tõnis Koppel <t6niskoppel@gmail.com>
2016-10-13 01:50:07 +0300

    ok

--- Box fort wars/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Box fort wars/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -16,6 +16,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         public float smoothTime = 5f;
         public bool lockCursor = true;
 
+
         private Quaternion m_CharacterTargetRot;
         private Quaternion m_CameraTargetRot;
         private bool m_cursorIsLocked = true;
